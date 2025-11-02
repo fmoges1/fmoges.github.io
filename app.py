aa22bd2e6a687270ca2800ea2a840512793fd664
fmoges1 <fmoges@gmail.com>
2025-11-02 13:16:46 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Loading Resume</title>
+        <script>
+            location.href = "https://fmoges1/fmoges.github.io/resume/your_resume.pdf";
+        </script>
+    </head>
+    <body>
+        Loading, please wait...
+    </body>
+    </html>
